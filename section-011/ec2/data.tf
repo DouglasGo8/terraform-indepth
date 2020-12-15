7181916b31d5a1d90b494424823100cb8f64461e
DouglasGo8 <sgtear@hotmail.com>
2020-12-15 08:24:31 -0300

    Module 11

--- section-011/ec2/data.tf
+++ section-011/ec2/data.tf
@@ -1,24 +1,24 @@
 
 data "aws_vpc" "default" {
-    default = true
+  default = true
 }
 
 data "aws_subnet_ids" "all" {
-    vpc_id = data.aws_vpc.default.id
+  vpc_id = data.aws_vpc.default.id
 }
 
 data "aws_ami" "amazon_linux" {
-    most_recent = true
-    filter {
-        name = "name"
-        values = [
-            "amzn-ami-hvm-*-x86_64-gp2"
-        ]
-    }
-    filter {
-      name = "owner-alias"
-      values = [
-          "amazon"
-      ]
-    }
+  most_recent = true
+  filter {
+    name = "name"
+    values = [
+      "amzn-ami-hvm-*-x86_64-gp2"
+    ]
+  }
+  filter {
+    name = "owner-alias"
+    values = [
+      "amazon"
+    ]
+  }
 }
